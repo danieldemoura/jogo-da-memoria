c7386ad4fe3e212415d45a446c8a8e7787b5ea32
Daniel de Moura Silva <nazuyamikima@gmail.com>
2023-09-29 11:39:48 -0300

    :tada: initial commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,60 @@
+const emojis = [
+    "🧘‍♂️",
+    "🧘‍♂️",
+    "🤯",
+    "🤯",
+    "💵",
+    "💵",
+    "🥶",
+    "🥶",
+    "👾",
+    "👾",
+    "💻️",
+    "💻️",
+    "🚀",
+    "🚀",
+    "🛸",
+    "🛸",  
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.id = i;
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML
+        && openCards[0].id !== openCards[1].id
+    ) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Você venceu!")
+    }
+}
